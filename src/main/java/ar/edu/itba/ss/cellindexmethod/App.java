31495226ee4c82cce777f862391a1f789ea4678d
EnriqueTawara <etawara@itba.edu.ar>
2021-08-13 08:20:00 +0900

    removed temp file, modified erroneous [ms] from method name

--- src/main/java/ar/edu/itba/ss/cellindexmethod/App.java
+++ src/main/java/ar/edu/itba/ss/cellindexmethod/App.java
@@ -6,9 +6,8 @@ import java.time.Instant;
 import java.util.*;
 
 
-public class App
-{
-    public static void main( String[] args ) throws IOException {
+public class App {
+    public static void main(String[] args) throws IOException {
         List<Particle> particleList = new ArrayList<>();
 
         File staticFile = new File("C:\\Users\\Laptop\\Desktop\\ITBASS2021Q2G8_TP01\\inputGenerator\\StaticFile.txt");
@@ -39,22 +38,21 @@ public class App
         String[] splitStaticLinesRadius = staticLineRadius.split("\\t+");
         Double r = Double.parseDouble(splitStaticLinesRadius[0]);
 
-
-        BruteForceMethod bf = new BruteForceMethod(N,L,r,particleList);
+        BruteForceMethod bf = new BruteForceMethod(N, L, r, particleList);
         // calculo el tiempo que tarda en ejecutar
         Instant startTimeBF = Instant.now();
         Map<Particle, Set<Particle>> resultsBF = bf.algorithm();
         Instant endTimeBF = Instant.now();
-        Duration totalTimeBF = Duration.between(startTimeBF,endTimeBF);
+        Duration totalTimeBF = Duration.between(startTimeBF, endTimeBF);
 
         FileWriter closestsParticlesBF = new FileWriter("outputBF.txt");
 
-        closestsParticlesBF.write("Method:\t" + BruteForceMethod.methodKey + "\t[ms]\n");
+        closestsParticlesBF.write("Method:\t" + BruteForceMethod.methodKey + "\n");
         closestsParticlesBF.write("Total time:\t" + totalTimeBF.toMillis() + "\t[ms]\n");
         // itero por todas las claves
-        for(Particle particle : resultsBF.keySet()) {
+        for (Particle particle : resultsBF.keySet()) {
             // si la partícula tiene valores asociados (partículas cercanas) las escribo en el output
-            if( resultsBF.get(particle).size() > 0 ) {
+            if (resultsBF.get(particle).size() > 0) {
                 closestsParticlesBF.write("[");
                 closestsParticlesBF.write(String.valueOf(particle.getParticleId()));
                 // itero por todas las partículas cercanas asociadas con la key
@@ -65,25 +63,24 @@ public class App
                 closestsParticlesBF.write("\n");
             }
         }
-        System.out.println("Salida BF generada");
+        System.out.println("Salida" + BruteForceMethod.methodKey + "generada");
         closestsParticlesBF.close();
-
 
         int m = 10;
         CellIndexMethod cim = new CellIndexMethod(m, L, r, true);
         Instant startTimeCim = Instant.now();
         cim.run(particleList);
         Instant endTimeCim = Instant.now();
-        Duration totalTimeCim = Duration.between(startTimeCim,endTimeCim);
+        Duration totalTimeCim = Duration.between(startTimeCim, endTimeCim);
 
         FileWriter closestsParticlesCim = new FileWriter("outputCim.txt");
 
-        closestsParticlesCim.write("Method:\t" + CellIndexMethod.methodKey + "\t[ms]\n");
+        closestsParticlesCim.write("Method:\t" + CellIndexMethod.methodKey + "\n");
         closestsParticlesCim.write("Total time:\t" + totalTimeCim.toMillis() + "\t[ms]\n");
         // itero por todas las claves
-        for(Particle particle : particleList) {
+        for (Particle particle : particleList) {
             // si la partícula tiene valores asociados (partículas cercanas) las escribo en el output
-            if( particle.hasNeighbor()) {
+            if (particle.hasNeighbor()) {
                 closestsParticlesCim.write("[");
                 closestsParticlesCim.write(String.valueOf(particle.getParticleId()));
                 // itero por todas las partículas cercanas asociadas con la key
@@ -94,9 +91,7 @@ public class App
                 closestsParticlesCim.write("\n");
             }
         }
-        System.out.println("Salida CIM generada");
+        System.out.println("Salida" + CellIndexMethod.methodKey + "generada");
         closestsParticlesCim.close();
-
-
     }
 }
