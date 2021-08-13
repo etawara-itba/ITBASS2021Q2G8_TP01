cf914a9cfa8b963ee19c79e249f4860f2d42b710
Ciccio <mciccioli@itba.edu.ar>
2021-08-12 23:46:20 -0300

    get best M added

--- src/main/java/ar/edu/itba/ss/cellindexmethod/App.java
+++ src/main/java/ar/edu/itba/ss/cellindexmethod/App.java
@@ -9,12 +9,17 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws IOException {
         List<Particle> particleList = new ArrayList<>();
+        double maxRadius = 0;
 
         File staticFile = new File("C:\\Users\\Laptop\\Desktop\\ITBASS2021Q2G8_TP01\\inputGenerator\\StaticFile.txt");
         File dynamicFile = new File("C:\\Users\\Laptop\\Desktop\\ITBASS2021Q2G8_TP01\\inputGenerator\\DynamicFile.txt");
 
         BufferedReader staticReader = new BufferedReader(new FileReader(staticFile));
         BufferedReader dynamicReader = new BufferedReader(new FileReader(dynamicFile));
+
+
+
+
 
         int N = Integer.parseInt(staticReader.readLine());
         int L = Integer.parseInt(staticReader.readLine());
@@ -25,6 +30,9 @@ public class App {
             String staticLine = staticReader.readLine();
             String[] splitStaticLines = staticLine.split("\\t+");
             radius = Double.parseDouble(splitStaticLines[0]);
+            if(radius > maxRadius){
+                maxRadius = radius;
+            }
             String dynamicLine = dynamicReader.readLine();
             String[] splitDynamicLines = dynamicLine.split("\\t+");
             coordX = Double.parseDouble(splitDynamicLines[0]);
@@ -38,7 +46,43 @@ public class App {
         String[] splitStaticLinesRadius = staticLineRadius.split("\\t+");
         Double r = Double.parseDouble(splitStaticLinesRadius[0]);
 
-        BruteForceMethod bf = new BruteForceMethod(N, L, r, particleList);
+
+        // Obtengo el óptimo número de celdas
+        int bestM = -1;
+        double radius1 = 0;
+        double radius2 = 0;
+        long timeForM = 10000000;
+        int M;
+        for(Particle particle : particleList) {
+            if(particle.getRadius() >= radius1){
+                radius2 = radius1;
+                radius1 = particle.getRadius();
+            } else if(particle.getRadius() > radius2)
+                radius2 = particle.getRadius();
+        }
+        // tomo L = 20, Rc = 1
+        double bound = 20/(1 + radius1 + radius2);
+        for(M = 1 + (int) bound; M > 0; M--) {
+            if(20 / M > (1 + radius1 + radius2)){
+                CellIndexMethod cimForM = new CellIndexMethod(M, 20 ,1, true);
+                Instant startForM = Instant.now();
+                cimForM.run(particleList);
+                Instant endForM = Instant.now();
+                Duration total = Duration.between(startForM,endForM);
+                if( total.toMillis() < timeForM){
+                    bestM = M;
+                    timeForM = total.toMillis();
+                }
+            }
+        }
+        System.out.println("Best M: " + bestM);
+
+
+
+
+
+
+        BruteForceMethod bf = new BruteForceMethod(N,L,r,particleList);
         // calculo el tiempo que tarda en ejecutar
         Instant startTimeBF = Instant.now();
         Map<Particle, Set<Particle>> resultsBF = bf.algorithm();
